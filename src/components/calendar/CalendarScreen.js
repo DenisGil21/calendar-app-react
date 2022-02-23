4e91b57c36c98fb0cc64e4018c1957792bbb127e
DenisGil21 <denisgilsil.21@gmail.com>
2022-02-23 12:15:33 -0600

    Diseño del front terminado

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,86 @@
+import { Navbar } from "../ui/Navbar"
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from "../../helpers/calendar-messages-es";
+import 'moment/locale/es-mx';
+import { CalendarEvent } from "./CalendarEvent";
+import { useState } from "react";
+import { CalendarModal } from "./CalendarModal";
+import { useDispatch, useSelector } from "react-redux";
+import { uiOpenModal } from "../../actions/ui";
+import { eventClearActiveEvent, eventSetActive } from "../../actions/events";
+import { AddNewFab } from "../ui/AddNewFab";
+import { DeleteEventFab } from "../ui/DeleteEventFab";
+
+moment.locale('es');
+
+const localizer = momentLocalizer(moment);
+
+export const CalendarScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const { events, activeEvent } = useSelector(state => state.calendar);
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const onDoubleClick = (e) => {
+        dispatch(uiOpenModal());
+    }
+
+    const onSelectEvent = (e) => {
+        dispatch(eventSetActive(e));
+    }
+
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
+
+    const onSelectSlot = (e) => {
+        dispatch(eventClearActiveEvent());
+    }
+
+    const eventSyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    }
+
+    return (
+        <div className="calendar-screen">
+            <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventSyleGetter}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                onSelectSlot={onSelectSlot}
+                selectable={true}
+                view={lastView}
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+            <AddNewFab />
+            {
+                activeEvent &&
+                <DeleteEventFab />
+            }
+            <CalendarModal />
+        </div>
+    )
+}
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4">
+        <span className="navbar-brand">
+            Denis
+        </span>
+        <button className="btn btn-outline-danger me-2">
+            <span className="fas fa-sign-out-alt "> Salir</span>
+        </button>
+    </div>
+  )
+}
